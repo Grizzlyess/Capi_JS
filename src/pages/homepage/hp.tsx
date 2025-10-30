5a8d365b6ff011c8aa7f4d0471ef9767b0745508
Agliandro <aragornrings7@gmail.com>
2025-10-29 21:19:04 -0300

    alguns testes

--- src/pages/homepage/hp.tsx
+++ src/pages/homepage/hp.tsx
@@ -1,5 +1,19 @@
-function Homepage {
-    return(
-        <div>Hello world</div>
+import React from 'react'
+import './hp.css'
+
+const HomePage = () => {
+    return (
+        <>
+            <header>
+                <h1>Teste</h1>
+            </header>
+            <main>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, nulla officia eveniet perferendis omnis deserunt. Delectus odit cupiditate itaque hic officia, natus ipsa voluptatum! Reiciendis nemo maiores neque aliquid exercitationem?</p>
+            </main>
+            <footer>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, vitae delectus expedita sint quibusdam officiis asperiores eaque ducimus sunt, veniam optio tempore itaque, officia exercitationem atque soluta provident? Accusamus, tempore!
+            </footer>
+        </>
     )
 }
+export default HomePage
