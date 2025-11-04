cc01c7a645dd2ba7e52688430071fe4868815936
Agliandro <aragornrings7@gmail.com>
2025-11-04 14:40:11 -0300

    Estruturando a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomePage from './pages/homepage/hp'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
---
+++ src/components/Main.tsx
@@ -0,0 +1,20 @@
+const Main = () => {
+    return(
+        <main>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid beatae, ea ut illo explicabo unde quo nihil eos neque! Deleniti nulla saepe animi cupiditate, ut quis cum beatae earum ad.</p>
+            <section>
+                Imagem 1
+                <img src="" alt="" />
+            </section>
+            <section>
+                Imagem 2
+                <img src="" alt="" />
+            </section>
+            <section>
+                Imagem 3
+                <img src="" alt="" />
+            </section>
+        </main>
+    )
+}
+export default Main
