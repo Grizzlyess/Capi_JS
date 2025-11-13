099327f3216a9e1b60b37c36b6d96d2a19bc99c6
LevySena <marcoslevysena10@gmail.com>
2025-11-13 16:28:43 -0300

    Adicionando rota GET de empresas do banco

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 //Backend modulos
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-app.get("/api",(req,res) => {
-    res.json({"fruits":["maça","banana","uva","morango","pera"]})
-})
-app.listen(8080,() => console.log("Server iniciado na porta 8080"))
+app.get("/emps", async (req, res) => {
+  const prisma = new PrismaClient();
+  res.send(await prisma.empresa.findMany());
+});
+
+app.get("/api", (req, res) => {
+  res.json({ fruits: ["maça", "banana", "uva", "morango", "pera"] });
+});
+app.listen(8080, () => console.log("Server iniciado na porta 8080"));
