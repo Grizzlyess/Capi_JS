224fc03070faa02b917bf21a89d41da1248129fe
LevySena <marcoslevysena10@gmail.com>
2025-11-14 14:21:31 -0300

    Alterando gradiente e estilizando a tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,32 +1,31 @@
 import "./Login.css";
 
 const Login = () => {
-  return (
-    <main className="main d-flex justify-content-center align-items-center min-vh-100 flex-column">
-      <div className="logo text-center mb-5">
-        <img src="src/assets/capi.svg" alt="" />
-      </div>
-      <div className="login bg-primary p-4">
-        <h3 className="mb-3">CAPI - Login</h3>
-        <form>
-          <div className="form-floating mb-3">
-            <input type="email" className="form-control" id="floatingInput" />
-            <label className="form-label">Email address</label>
-          </div>
-          <div className="form-floating mb-5">
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-            <label className="form-label">Password</label>
-          </div>
-          <button type="submit" className="btn btn-info w-100">
-            Login
-          </button>
-        </form>
-      </div>
-    </main>
-  );
+    return (
+        <div className="main d-flex justify-content-center align-items-center min-vh-100 flex-column">
+            <div className="logo text-center mb-5">
+                <img src="src/assets/capi.svg" alt="" />
+            </div>
+            <div className="login p-4">
+                <h3 className="mb-5">CAPI - Login</h3>
+                <div className="forms">
+                    <div className="form mb-4">
+                        <input type="email" className="form-control" placeholder="Email" />
+                    </div>
+                    <div className="form mb-5">
+                        <input type="password" className="form-control" placeholder="Senha" />
+                    </div>
+                    <button type="submit" className="btn btn-info w-100 mb-3">
+                        Login
+                    </button>
+                    <div className="register">
+                        <p>
+                            Não tem conta? <a href="#">Registre-se</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Login;
