d8b51049c1e0777d7b4826370429ee040d0af762
Agliandro <aragornrings7@gmail.com>
2025-11-19 21:41:39 -0300

    alguns ajustes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,22 +3,23 @@ import "./Login.css"
 const Login = () =>{
     return(
         <main className="d-flex justify-content-center align-items-center min-vh-100 flex-column">
-            <div className="logo text-center mb-5">
+            <div className="logo text-center mb-3">
                 <img src="src/assets/capi.ico" alt="" />
             </div>
-            <div className="login bg-primary p-4">
-                <h3 className="mb-3">CAPI - Login</h3>
-                <form>
-                    <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="floatingInput"/>
-                        <label for="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating mb-5">
-                        <input type="password" className="form-control" id="exampleInputPassword1"/>
-                        <label for="exampleInputPassword1" className="form-label">Password</label>
-                    </div>
-                    <button type="submit" className="btn btn-info w-100">Login</button>
-                </form>
+            <div className="login p-3">
+                <h4 className="mb-5">CAPI - Login</h4>
+                
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="email"/>
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating mb-5">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                    <label htmlFor="floatingPassword" className="form-label">Password</label>
+                </div>
+
+                <button type="submit" className="btn btn-info w-100">Login</button>
+                
             </div>
         </main>
     )
