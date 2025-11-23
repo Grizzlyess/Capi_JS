9041985702138d832b942e7c8ee7211b06fd6d7c
Grizzyless <mariaclarab316@gmail.com>
2025-11-23 20:42:09 -0300

    Implementado a calculadora, ainda precisa de ajustes

---
+++ backend/routes/calculoRoutes.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+
+router.post('/', async (req, res) => {
+  const { userId, resultado } = req.body;
+
+  try {
+    const novoCalculo = await prisma.calculoCarbono.create({
+      data: {
+        userId: userId,
+        valorTotal: parseFloat(resultado),
+      },
+    });
+
+    const calculosDoUsuario = await prisma.calculoCarbono.findMany({
+      where: { userId: userId },
+      orderBy: { createdAt: 'asc' }, 
+    });
+
+
+    if (calculosDoUsuario.length > 2) {
+      const calculoMaisAntigo = calculosDoUsuario[0];
+      
+      await prisma.calculoCarbono.delete({
+        where: { id: calculoMaisAntigo.id }
+      });
+      
+      console.log(`${calculoMaisAntigo.id} foi deletado`);
+    }
+
+    res.status(201).json(novoCalculo);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao salvar" });
+  }
+});
+
+router.get('/user/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const calculos = await prisma.calculoCarbono.findMany({
+      where: { userId: userId },
+      orderBy: { createdAt: 'desc' }
+    });
+
+    res.json(calculos);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar histórico" });
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import empresaRoutes from './routes/empresaRoutes.js';
+import calculoRoutes from './routes/calculoRoutes.js';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use("/user", userRoutes);
 app.use('/empresa', empresaRoutes);
+app.use('/calculo', calculoRoutes);
 
 app.listen(8080, () => console.log("Servidor iniciado na porta 8080"));
