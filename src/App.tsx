52024d52c3a52b7836d0cb9239859c3e84824e69
Agliandro <aragornrings7@gmail.com>
2025-12-03 15:01:04 -0300

    Card simples para listar as empresas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
 import Login from './pages/Login'
+import ListarEmpresas from './pages/ListarEmpresas'
 import "./App.css"
 
 function App() {
+
   return (
     <>
-      <Login />
+      <HomePage />
+      <Login/>
+      <ListarEmpresas data={{
+        nome: "teste",
+        localizacao: "Brasil",
+        setor: "Tecnologia",
+        anoTermo: "2020",
+        anoAlvo: "2024"
+  }}/>
     </>
   )
 }
