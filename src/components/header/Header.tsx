e8330e7cb3f99e2c2ab862a7d7d9c4a9e70984c6
Agliandro <aragornrings7@gmail.com>
2025-12-04 19:59:30 -0300

    Ajustes no Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => {
+ const Header = () => {
   const navigate = useNavigate()
   function OnLoginClick(){
     navigate("/login")
   }
   return (
     <header>
-      <div className="container text-center align-items-center">
-        <div className="row">
-          <div className="col ratio">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
             <img src="src/assets/capi.svg" id="logo" alt="logo-capi" />
-          </div>
-          <div className="col">
+
             <nav>Sobre</nav>
-          </div>
-          <div className="col">
+
             <button onClick={OnLoginClick}className="btn btn-success">Login</button>
-          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
