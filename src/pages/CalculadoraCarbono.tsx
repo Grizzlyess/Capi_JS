f294c45db224e4c570c307c43a0177b79913282b
Agliandro <aragornrings7@gmail.com>
2025-12-15 17:27:32 -0300

    Estrutura inicial da tela da calculadora de carbono

---
+++ src/pages/CalculadoraCarbono.tsx
@@ -0,0 +1,69 @@
+const CalculadoraCarbono = () =>{
+    return (
+        <>
+            <div className="container">
+                <p>Responda as questões</p>
+
+                <form action="">
+                    {/*Energia mensal*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+                    
+                    {/*Preço do kWh - região*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+
+                    {/*Qtd botijões - mês*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+
+                    {/*Tipo de veículo*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+
+                    {/*Tipo de combustível - carro*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+
+                    {/*Tipo de combustível - moto*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+
+                    {/*>Quilômetros que você anda por mês*/}
+                    <div className="mb-3">
+                        <label htmlFor="">
+
+                        </label>
+                        <input type="text" />
+                    </div>
+                </form>
+            </div>
+
+        </>
+    );
+}
+export default CalculadoraCarbono
