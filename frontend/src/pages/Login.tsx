761c2a782c8421ffd1623c846f5887fdf3137083
LevySena <marcoslevysena10@gmail.com>
2025-12-15 21:35:42 -0300

    Inicio de login com back

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
 
 const Login = () => {
-    const navigate = useNavigate()
-    function OnLoginClick(){
-        navigate("/cadastro")
-    }
+    const [email, setemail] = useState("");
+    const [pass, setPass] = useState("");
+    const navigate = useNavigate();
+    const toCad = () => {
+        navigate("/cadastro");
+    };
+
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //todo: if e else de autenticação usando axios (necessario criar rota)
+        localStorage.setItem("userToken", "token-aserdefinido");
+        localStorage.setItem("userInfo", JSON.stringify({ email }));
+        //else
+        alert("Email ou Senha errados");
+    };
+
     return (
         <div className="main d-flex justify-content-center align-items-center min-vh-100 flex-column">
             <div className="logo text-center">
@@ -13,22 +26,24 @@ const Login = () => {
             </div>
             <div className="login p-3">
                 <h3 className="mb-5">CAPI - Login</h3>
-                <div className="forms">
-                    <div className="form mb-4">
-                        <input type="email" className="form-control" placeholder="Email" />
+                <form onSubmit={handleLogin}>
+                    <div className="forms">
+                        <div className="form mb-4">
+                            <input type="email" className="form-control" placeholder="Email" />
+                        </div>
+                        <div className="form mb-5">
+                            <input type="password" className="form-control" placeholder="Senha" />
+                        </div>
+                        <button type="submit" className="btn logcad w-100 mb-3 mt-4">
+                            Login
+                        </button>
+                        <div className="register">
+                            <p>
+                                Não tem conta? <a onClick={toCad}>Registre-se</a>
+                            </p>
+                        </div>
                     </div>
-                    <div className="form mb-5">
-                        <input type="password" className="form-control" placeholder="Senha" />
-                    </div>
-                    <button type="submit" className="btn logcad w-100 mb-3 mt-4">
-                        Login
-                    </button>
-                    <div className="register">
-                        <p>
-                            Não tem conta? <a onClick={OnLoginClick}>Registre-se</a>
-                        </p>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
     );
