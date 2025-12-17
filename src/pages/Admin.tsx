6045fb928399676e100cba6f09d78d0f6b5aaad0
OViniSA <oviniciussilvaalves@gmail.com>
2025-12-17 12:34:16 -0300

    Estilização da Tela de Administrador

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,3 +1,5 @@
+import "./Admin.css"
+
 const Admin = () =>{
     return(
         <div className="d-flex flex-column justify-content-between min-vh-100">
@@ -11,33 +13,19 @@ const Admin = () =>{
                             <nav>Administrador</nav>
                         </div>
                         <div className="col">
-                            <button className="btn btn-success">Login</button>
+                            <button className="btn btn-success">Logout</button>
                         </div>
                     </div>
                 </div>
             </header>
-            <main className="d-flex flex-column flex-fill">
-                <div className="msg d-flex justify-content-center align-items-center">
-                    <p>O que fazemos com o planeta hoje determinará o nosso amanhã.</p>
-                </div>
-
-                <div className="container text-center mb-3">
-                    <div className="column">
-                        <div className="col p-1">
-                            <section className="ratio ratio-1x1">
-                                <button>Listar Usuários</button>
-                            </section>
-                        </div>
-                        <div className="col p-1">
-                            <section className="ratio ratio-1x1">
-                                <button>Atualizar Guia</button>
-                            </section>
-                        </div>
-                    </div>
+            <main className="flex-fill d-flex justify-content-center align-items-center">
+                <div className="d-flex flex-column gap-4">
+                        <button className="btn btn-lg botao">Listar Usuários</button>
+                        <button className="btn btn-lg botao">Atualizar Guia</button>
                 </div>
             </main>
             <footer>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque doloremque consectetur necessitatibus vero, aliquam consequuntur voluptate vitae vel fugit recusandae dolor, asperiores aliquid dolore porro deserunt expedita obcaecati inventore reiciendis.</p>
+                <p className="justify-content-center">©Capi. Created by: A Ordem, 2025.</p>
             </footer>
         </div>
     )
