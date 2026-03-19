06cd02ff4b058a230cfa19343efe48d3ab5e27f8
Fkaique <fkaiquesp@gmail.com>
2026-03-19 19:24:32 -0300

    fix: update CORS origin to remove unnecessary path in GitHub Pages URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: ['http://localhost:5173','https://grizzlyess.github.io/Capi_JS/'],
+        origin: ['http://localhost:5173','https://grizzlyess.github.io'],
         credentials: true,
     })
 );
